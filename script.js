5d55affadaf87cd4fcfa527dc5b387fbd92631c3
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-20 14:51:35 +0200

    generazione e stampa lista
    completato script per chiamare e stampare in pagina la lista di mail

--- script.js
+++ script.js
@@ -5,7 +5,17 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        mailList: []
       }
+    },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+                //console.log(response);
+                this.mailList.push(response.data.response);
+            })
+        }
     }
   }).mount('#app')
